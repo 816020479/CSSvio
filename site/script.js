8f55b28dbfe906b906ca1d9bde876694b776b4ac
816020479 <61168895+816020479@users.noreply.github.com>
2021-06-17 00:31:37 -0400

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,17 @@
+<script>
+			
+			var menu = document.getElementById('menu');
+			var menu = document.getElementById('nav');
+			var menu = document.getElementById('exit');
+
+			menu.addEventListener('click', function(e){
+				nav.classList.toggle('hide-mobile');
+				e.preventDefault();
+			});
+
+			exit.addEventListener('click', function(e){
+				nav.classList.add('hide-mobile');
+				e.preventDefault();
+			});
+
+</script>
